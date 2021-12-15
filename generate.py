870050e8d65a81724a63936de2b93bbc36d2c53a
mattgrayling <mjpg1g18@soton.ac.uk>
2021-12-15 14:41:38 +0000

    Removed old scripts and renamed classes to gan

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-from classes import GAN
+from gan import GAN
 
 gan = GAN(latent_dims=5, gen_activation='sigmoid', lr=0.0001, device='gpu:3', labels='soft', generator_type='rnn',
           cadence=None, data_type='sim', z_lim=0.08)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from classes import GAN
+from gan import GAN
 
 gan = GAN(latent_dims=5, gen_activation='sigmoid', lr=0.0001, device='gpu:0', labels='soft', generator_type='rnn',
           cadence=None, data_type='sim', z_lim=0.08, error=True, units=40)
